a886e94e624c8f18521339e2bbe715448115b82c
kimwoojoo <kyr1217@gmail.com>
2018-03-31 23:14:22 +0900

    Update MiniProJect.py

--- MiniProJect.py
+++ MiniProJect.py
@@ -1,7 +1,7 @@
 import Adafruit_DHT as dht
 import RPi.GPIO as GPIO
 import time
-
+import threading 
 #define Pin Number
 HumanPin = 17
 TempPin = 2
@@ -12,6 +12,22 @@ GreenPin = 21
 SwitchPin = 27
 TrigPin = 23
 EchoPin = 24
+
+class Check(threading.Thread):
+	def run(self):
+		CheckSenSor()
+	def stop(self):
+		self._stop.set()
+	def stopped(self):
+		return self._stop.isSet()
+
+class Check2(threading.Thread):
+	def run(self):
+		CheckSenSor2()
+	def stop(self):
+		self._stop.set()
+	def stopped(self):
+		return self._stop.isSet()
 
 #define Pin`s Setup 
 def Setup():
@@ -24,69 +40,73 @@ def Setup():
 	GPIO.setup(SwitchPin, GPIO.IN)
 	GPIO.setup(TrigPin, GPIO.OUT)
 	GPIO.setup(EchoPin, GPIO.IN)
-	
-#define Switch On and OFF Check and HumanSenSor Check
+
+
+#Grenn LED
 def CheckSenSor():
+	SwitchMode()
+
+
+#RED and Yellow LED
+
+def CheckSenSor2():
 	SwitchMode()
 	HuManSenSor()
 
-#define Red Led 
+
 def RLed():
-	print 'Red Led On'
-	CheckSenSor()
+	th = Check2()
+	th.start()
+	print 'Red Led On\n'
 	GPIO.output(RedPin, GPIO.HIGH)
 	time.sleep(3)
-	print 'Red Led OFF'
+	print 'Red Led OFF\n'
 	GPIO.output(RedPin, GPIO.LOW)
 	time.sleep(1)
 
-#define Green Led
 def GLed():
-	print 'Green Led On'
-	CheckSenSor()
+	Th = Check()
+	Th.start()
+	print 'Green Led On\n'
 	GPIO.output(GreenPin, GPIO.HIGH)
 	time.sleep(3)
-	print 'Green Led OFF'
+	print 'Green Led OFF\n'
 	GPIO.output(GreenPin, GPIO.LOW)
 	time.sleep(1)
 
-#define Yellow Led
 def YLed():
-	print 'Yellow Led On'
-	CheckSenSor()
+	th = Check2()
+	th.start()
+	print 'Yellow Led On\n'
 	GPIO.output(YellowPin, GPIO.HIGH)
 	time.sleep(3)
-	print 'Yellow Led OFF'
+	print 'Yellow Led OFF\n'
 	GPIO.output(YellowPin, GPIO.LOW)
 	time.sleep(1)
 
-#define HuManSenSor Detecting
 def HuManSenSor():
 	HValue = GPIO.input(HumanPin)
 	if(HValue == GPIO.HIGH):
-		print "Motion DeTecTing"
+		print "Motion DeTecTing\n"
 		printUltra()
 		GPIO.output(BuzzerPin, GPIO.HIGH)
 		time.sleep(1)
 		GPIO.output(BuzzerPin, GPIO.LOW)
 		time.sleep(1)
 
-#define Switch Push and Pull Detecting
 def SwitchMode():
 	SValue = GPIO.input(SwitchPin)
 	if(SValue == GPIO.HIGH):
-		print 'Switch On\nTemperature and Humidity print'
+		print 'Switch On\nTemperature and Humidity print\n'
 		printTempHumi()
 		
 	return SValue
 
-#define Print Temperature and Humidity 
 def printTempHumi():
 	time.sleep(0.5)
 	h,t = dht.read_retry(dht.DHT11, TempPin)
-	print 'Temperature = {0:0.1f}*C\n Humidity = {1:0.1f}%'.format(t,h)
+	print 'Temperature = {0:0.1f}*C\n Humidity = {1:0.1f}%\n'.format(t,h)
 
-#define Print Distance
 def printUltra():
 	GPIO.output(TrigPin, GPIO.LOW)
 	time.sleep(0.3)
@@ -103,14 +123,11 @@ def printUltra():
 	Distance = pulse_duration * 17150
 	Distance = round(Distance,2)
 
-	print "Distance Between Human Sensor and Person : ", Distance , "CM" 
-	
-#define Main Method
+	print "Distance Between Human Sensor and Person : ", Distance , "CM\n" 
+
 try:
 	while True:
-#PinSetup
 		Setup()
-#Each Led Method Call
 		RLed()
 		time.sleep(1)
 		YLed()
@@ -121,10 +138,4 @@ except KeyboardInterrupt:
 	GPIO.cleanup()
 	print 'GoodBye'
 
-'''
-while True:
-	h, t = dht.read_retry(dht.DHT11, TempPin)
-	print 'temp = {0:0.1f}*C Humidity = {1:0.1f}%'.format(t,h)
-	time.sleep(1)
-'''
 
